9226792d5b990cf1d3fbb02875f355b38f4d96a5
Amaar Mohamed <codebyamaar@gmail.com>
2022-07-19 15:58:39 -0500

    added Skeleton for Userpage, Admin Panel, Also added routes for both in app.jsx

--- Flora-Fauna-Frontend/src/components/App/App.jsx
+++ Flora-Fauna-Frontend/src/components/App/App.jsx
@@ -8,18 +8,19 @@ import Navbar from '../Navbar/Navbar'
 import NotFound from '../NotFound/NotFound'
 import RegistrationPage from '../RegistrationPage/RegistrationPage'
 import AdminOverview from "../AdminOverview/AdminOverview.jsx"
+import UserFeed from "../UserFeed/UserFeed.jsx"
 import './App.css'
 
-import { AuthContextProvider } from "../../contexts/auth.jsx";     
+import { AuthContextProvider } from "../../contexts/auth.jsx";
 
 
 
 export default function AppContainer() {
-   return (
-      <AuthContextProvider>
-         <App/>
-      </AuthContextProvider>
-   )
+  return (
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  )
 }
 
 function App() {
@@ -28,16 +29,17 @@ function App() {
   const [error, setError] = useState(null)
   const [isFetching, setIsFetching] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
-   
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar isLoading={isLoading} />
         <Routes>
-          <Route path="/" element={<LandingPage />}/>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="/admin" element={<AdminOverview />} />
+          <Route path="/userfeed" element={<UserFeed />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/denied" element={<AccessForbidden />} />
         </Routes>

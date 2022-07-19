ba82b63bb230ac3b307a4cf3c735dbdb7defea58
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2022-07-19 17:19:13 -0400

    user model and auth routes

--- Flora-Fauna-Backend/routes/auth.js
+++ Flora-Fauna-Backend/routes/auth.js
@@ -5,22 +5,34 @@ const { createUserJwt } = require("../utils/tokens");
 
 router.post("/login", async(req,res,next) => {
     try{
-        const user = await User.login(req.body);
-        const token = createUserJwt(user);
-        return res.status(200).json({ user, token });
+        const user = await User.login(req.body)
+        const token = createUserJwt(user)
+        return res.status(200).json({ user, token })
     } catch(err){
-        next(err);
+        next(err)
     }
 })
 
 router.post("/register", async(req,res,next) => {
     try {
-        const user = await User.register(req.body);
-        const token = createUserJwt(user);
-        return res.status(200).json({ user, token });
+        const user = await User.register(req.body)
+        const token = createUserJwt(user)
+        return res.status(200).json({ user, token })
     }  
     catch(err){
-       next(err);
+       next(err)
+    }
+})
+
+router.get(("/me"), async(req,res,next) => {
+    try {
+        const { email } = res.locals.user;
+        const user = await User.fetchUserByEmail(email);
+        const publicUser = {id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name}
+        return res.status(200).json({ user: user })
+    }
+    catch(err){
+        next(err)
     }
 })
 module.exports = router

d819548032fe108a8ba94d5d341a139edbde7359
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2022-07-21 16:05:13 -0400

    missed comments

--- Flora-Fauna-Backend/models/planimal.js
+++ Flora-Fauna-Backend/models/planimal.js
@@ -22,9 +22,9 @@ class Planimal {
     static async getResults(searchInput) {
         API_TOKEN=`exrEXXrEuN2u9UchWxUidMwQ5`
         GENERIC_FILTER=`$select=distinct common_name,scientific_name,taxonomic_group,state_conservation_rank&$where=category not in('Natural Community')`
-        //PARSED_TERMS= Planimal.modifyTerms(searchInput)
+        PARSED_TERMS= Planimal.modifyTerms(searchInput)
         
-        //let url = `https://data.ny.gov/resource/tk82-7km5.json?$$app_token=${API_TOKEN}&${GENERIC_FILTER} AND common_name like "${PARSED_TERMS}"`
+        let url = `https://data.ny.gov/resource/tk82-7km5.json?$$app_token=${API_TOKEN}&${GENERIC_FILTER} AND common_name like "${PARSED_TERMS}"`
         const response = await fetch(url)
         const myJson = await response.json()
         console.log(myJson)

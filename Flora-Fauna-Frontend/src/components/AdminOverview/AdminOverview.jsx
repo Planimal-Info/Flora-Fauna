b0e388733fd888b1ee6c67e8fe2d1cd70398ab3e
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-07-22 13:20:44 -0400

    starter admin dashboard

--- Flora-Fauna-Frontend/src/components/AdminOverview/AdminOverview.jsx
+++ Flora-Fauna-Frontend/src/components/AdminOverview/AdminOverview.jsx
@@ -1,15 +1,53 @@
 import "./AdminOverview.css";
 import { useAuthContext } from "../../contexts/auth";
+import { useState } from "react";
 
 export default function AdminOverview(props) {
   // const { user } = useAuthContext();
+  const [togglePosts, setTogglePosts] = useState(true)
+  const [toggleUsers, setToggleUsers] = useState(false)
+  
+
+  const adminPostsHandler = () => {
+    setTogglePosts(true)
+    setToggleUsers(false)
+  }
+
+  const adminUsersHandler = () => {
+    setTogglePosts(false)
+    setToggleUsers(true)
+  }
+
+
   return (
     <div className="admin-overview">
-      <h2>Admin Panel</h2>
-      <div className="admin-navbar">
-        <h4 className="admin-navbar-link">Flagged Posts</h4>
-        <h4 className="admin-navbar-link">Flagged Users</h4>
-      </div>
+        <div className="content">
+          <div className="column left">
+            <div className="admin-panel">
+              <h2>Admin Panel</h2>
+              <ul>
+                <li>
+                  <p onClick={adminPostsHandler}>Flagged Posts</p>
+                  <p onClick={adminUsersHandler}>Flagged Users</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="column right">
+            <div className="admin-header">
+              
+            </div>
+            <div className="flagged-posts">
+              {togglePosts && <AdminFlaggedPosts />}
+            </div>
+            <div className="flagged-users">
+              {toggleUsers && <AdminFlaggedUsers />}
+            </div>
+          </div>
+        </div>
+        {/* <h4 className="admin-navbar-link">Flagged Posts</h4>
+        <h4 className="admin-navbar-link">Flagged Users</h4> */}
     </div>
   )
 }

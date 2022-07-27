d86926b333451de62c8547ab6c5290286e974d32
Amaar Mohamed <codebyamaar@gmail.com>
2022-07-27 14:15:12 -0500

    Made user cards and displayed that in user feed. Also added upload button on user feed page. Some styling, but nothing too much. Also added calls for getting posts, sending posts and images, and added comments

--- Flora-Fauna-Frontend/src/components/UploadPage/UploadPage.jsx
+++ Flora-Fauna-Frontend/src/components/UploadPage/UploadPage.jsx
@@ -1,16 +1,19 @@
 import "./UploadPage.css";
-import { useRef } from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { usePostContext } from "../../contexts/posts.jsx";
 
 export default function UploadPage() {
-  
+  const { createPost, posts, bufferToBase64 } = usePostContext();
+  const navigate = useNavigate();
+
   //const [fileImage, setFilemage] = useState({})
   const [selectedImage, setSelectedImage] = useState();
   const [uploadValues, setUploadValues] = useState({
     imageUrl: "",
     title: "",
-    caption: ""
-  })
+    caption: "",
+  });
 
   // Handle value changes for image url, title and caption
   const handleUploadChange = (e) => {
@@ -18,55 +21,70 @@ export default function UploadPage() {
       ...uploadValues,
       [e.target.name]: e.target.value,
     });
-  }
+  };
   //////////////////////
 
+  //Updates image when changes happen
   const imageChange = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       setSelectedImage(e.target.files[0]);
     }
   };
-
+  
+  //Removes image preview if clicked on
   const removeSelectedImage = () => {
     setSelectedImage();
   };
 
   // open file explorer/finder to select image
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
 
   const handleClick = () => {
     inputRef.current.click();
-  }
+  };
 
-  const handleFileChange = e => {
+  //Updates changes for file values
+  const handleFileChange = (e) => {
     const fileObj = e.target.files && e.target.files[0];
     if (!fileObj) {
       return;
     }
-  }
+  };
+  
+  //Submit function that is called when user submits form
+  const handleOnSubmit = () => {
+    const inputObj = { image: selectedImage, values: uploadValues };
+    createPost(inputObj);
+    navigate('/userfeed')
+  };
   //e.target.value = null
   ////////////////////
-
+ 
   return (
     <div className="upload-page">
       <div className="upload-form">
-        <div className="upload-header">Upload Post</div>
-        {/* SELECT IMAGE FILE FROM COMPUER */}
-        <input 
-          type="file"
-          name="fileUpload"
-          ref={inputRef}
-          onChange={imageChange}
-          hidden
+        <form enctype="multipart/form-data">
+          <div className="upload-header">Upload Post</div>
+          {/* SELECT IMAGE FILE FROM COMPUER */}
+          <input
+            type="file"
+            name="file"
+            ref={inputRef}
+            onChange={imageChange}
+            hidden
           >
           </input>
+        </form>
         <div className="file-upload" onClick={handleClick}>
           <div className="material-symbols-outlined add-file">note_add</div>
           <h2>Select an image file to upload</h2>
         </div>
         {selectedImage && (
           <div className="preview-image">
-            <img className="uploaded-preview-image" src={URL.createObjectURL(selectedImage)} />
+            <img
+              className="uploaded-preview-image"
+              src={URL.createObjectURL(selectedImage)}
+            />
             <button className="remove-image" onClick={removeSelectedImage}>
               <span class="material-symbols-outlined cancel">cancel</span>
             </button>
@@ -76,14 +94,16 @@ export default function UploadPage() {
         <UploadImageUrl handleUploadChange={handleUploadChange} />
 
         {/* INSERT TITLE */}
-        <div className="user-post-title">
-          <label htmlFor="post-title">Title:</label>
-          <input
-            type="text"
-            name="title"
-            onChange={handleUploadChange} />
-        </div>
-
+        <form enctype="multipart/form-data">
+          <div className="user-post-title">
+            <label htmlFor="post-title">Title:</label>
+            <input
+              type="text"
+              name="title"
+              onChange={handleUploadChange}
+            />
+          </div>
+        </form>
         {/* INSERT CAPTION */}
         <div className="user-caption">
           <label htmlFor="caption">Insert Caption:</label>
@@ -95,36 +115,38 @@ export default function UploadPage() {
             placeholder="Enter text here..."
             maxLength="300"
             onChange={handleUploadChange}
-            ></textarea>
+          >
+          </textarea>
         </div>
-        <button className="post-submit btn">Submit</button>
+        <button className="post-submit btn" onClick={handleOnSubmit}>
+          Submit
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 // Component to upload image via image url
 export function UploadImageUrl(props) {
+  const { handleUploadChange } = props;
 
-  const { handleUploadChange } = props
-
-  return(
+  return (
     <div className="upload-image-url">
       <div className="url-upload">
-          <label className="image-url-upload">Or upload from URL</label>
+        <label className="image-url-upload">Or upload from URL</label>
 
-          <div className="url-input">
-            <input
-              id="image-url"
-              type="text"
-              name="imageUrl"
-              autoComplete="false"
-              autofill="off"
-              onChange={handleUploadChange}
-              />
-            <button id="image-submit" className="upload btn">Upload</button>
-          </div>
+        <div className="url-input">
+          <input
+            id="image-url"
+            type="text"
+            name="imageUrl"
+            autoComplete="false"
+            autofill="off"
+            onChange={handleUploadChange}
+          />
+          <button id="image-submit" className="upload btn">Upload</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ Flora-Fauna-Frontend/src/components/UserCards/UserCards.jsx
@@ -0,0 +1,11 @@
+import "./UserCards.css";
+
+export default function UserCards(props){
+  return(
+    <div className="user-card">
+    <img src={props.source} className="user-img"/>
+    <h2 className="user-card-title">{props.title}</h2>
+    <h4 className="user-card-desc">{props.desc}</h4>
+    </div>
+  )
+}

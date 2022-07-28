8df58de131dd11deceeed4cf66a9b79a6fa01172
Amaar Mohamed <codebyamaar@gmail.com>
2022-07-28 17:49:52 -0500

    Made loading posts faster, now only loads a limited amount with a option to load more, also refractored useEffects to be more effective

--- Flora-Fauna-Frontend/src/contexts/posts.jsx
+++ Flora-Fauna-Frontend/src/contexts/posts.jsx
@@ -4,7 +4,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 const PostContext = createContext(null);
 
 export const PostContextProvider = ({ children }) => {
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState([]);
+  const [latestPost, setLatestPost] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [initialized, setInitialized] = useState(false);
   const [error, setError] = useState({});
@@ -12,8 +13,8 @@ export const PostContextProvider = ({ children }) => {
   //Grabs the posts when the component renders
   useEffect(async () => {
     try {
-      const data = await ApiClient.getAllPosts();
-      setPosts(data?.data?.allPosts);
+      const data = await ApiClient.getInitialPosts();
+      setPosts(data.data.initialPosts);
     } catch (err) {
       console.error(err);
     }
@@ -25,19 +26,42 @@ export const PostContextProvider = ({ children }) => {
     setInitialized(false);
     try {
       const getData = await ApiClient.createPost(data);
-      setPosts(getData?.addImage);
+      setLatestPost(getData?.addImage);
     } catch (err) {
       console.error(err);
     }
     setIsLoading(false);
     setInitialized(true);
   };
+
+  //Gets more posts to display when prompted
+  const getMorePosts = async (post_id) => {
+    try {
+      const data = await ApiClient.getMorePosts(post_id);
+      console.log(data);
+      if (data.data.getMore.length > 0) {
+        const arr = [];
+        posts.forEach(e => {
+          arr.push(e);
+        })
+        data.data.getMore.forEach(e => {
+          arr.push(e);
+        })
+        setPosts(arr);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const postValue = {
     posts,
     isLoading,
     initialized,
     error,
     createPost,
+    latestPost,
+    getMorePosts,
   };
 
   return (

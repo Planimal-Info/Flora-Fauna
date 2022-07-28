03d08bac0b220bc2f0354ad9bddd67104d0dabb7
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2022-07-28 13:14:26 -0400

    calls WikiMedia Api for wildlife photos

--- Flora-Fauna-Backend/models/planimal.js
+++ Flora-Fauna-Backend/models/planimal.js
@@ -1,6 +1,7 @@
 const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 const db = require("../db");
 const { API_KEY, PEXEL_API_KEY } = require("../config.js");
+const { json } = require("express");
 
 class Planimal {
   //Changes each term to be begin with a capital letter
@@ -17,6 +18,30 @@ class Planimal {
     return "%25" + joinedWord + "%25";
   }
 
+  //Fetches the photos from wikipedia API and displays that.
+  static async getPhotoResults(searchInput) {
+
+    const FirstCallUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchInput.data}&limit=5&namespace=0&format=json&redirects=resolve`;
+    const SecondCallUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=`;
+
+    const data = await fetch(FirstCallUrl);
+    const jsonData = await data.json();
+
+    const wikiLink = jsonData.pop();
+    // array empty or does not exist
+    if (wikiLink.length == 0) {
+      return "";
+    }
+    else {
+      
+      let wikiLinkSub = wikiLink[0].substring(wikiLink[0].lastIndexOf("/")+1);
+      const secondCallData = await fetch(SecondCallUrl+wikiLinkSub);
+      const secondJsonData = await secondCallData.json();
+
+      return secondJsonData.query.pages[0].original.source;
+    }
+  }
+
   //fetches data from api and returns a json file
   //TODO - add numeric check withen search term to prevent errors
   static async getResults(searchInput) {
@@ -30,23 +55,6 @@ class Planimal {
     const myJson = await response.json();
 
     return myJson;
-  }
-
-  //Fetches the photos from pexel API and displays that.
-  static async getPhotoResults(searchInput) {
-    const data = await fetch(
-      `https://api.pexels.com/v1/search?query=${searchInput.query}`,
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: PEXEL_API_KEY, //use the apikey you have generated
-        },
-      },
-    );
-
-    const jsonData = await data.json();
-    return jsonData
   }
 }
 

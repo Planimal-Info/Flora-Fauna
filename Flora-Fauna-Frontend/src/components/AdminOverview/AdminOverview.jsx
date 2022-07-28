89a46049d8dec9a1c9fd7f936eb9533a9bee5be4
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-07-28 13:16:03 -0400

    admin notification foundation for flagged posts/users

--- Flora-Fauna-Frontend/src/components/AdminOverview/AdminOverview.jsx
+++ Flora-Fauna-Frontend/src/components/AdminOverview/AdminOverview.jsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 
 export default function AdminOverview(props) {
   // const { user } = useAuthContext();
+
   const [togglePosts, setTogglePosts] = useState(true)
   const [toggleUsers, setToggleUsers] = useState(false)
-  
+  const [deleteItem, setDeleteItem] = useState(false)
 
   const adminPostsHandler = () => {
     setTogglePosts(true)
@@ -19,6 +20,9 @@ export default function AdminOverview(props) {
     setToggleUsers(true)
   }
 
+  const handleDeleteToggle = () => {
+    setDeleteItem(!deleteItem)
+  }
 
   return (
     <div className="admin-overview">
@@ -45,10 +49,10 @@ export default function AdminOverview(props) {
             </div>
             <div className="admin-body">
               <div className="flagged-posts">
-                {togglePosts && <AdminFlaggedPosts />}
+                {togglePosts && <AdminFlaggedPosts deleteItem={deleteItem} setDeleteItem={setDeleteItem} handleDeleteToggle={handleDeleteToggle} />}
               </div>
               <div className="flagged-users">
-                {toggleUsers && <AdminFlaggedUsers />}
+                {toggleUsers && <AdminFlaggedUsers deleteItem={deleteItem} setDeleteItem={setDeleteItem} handleDeleteToggle={handleDeleteToggle} />}
               </div>
             </div>
           </div>
@@ -62,8 +66,9 @@ export default function AdminOverview(props) {
 //Implement when backend authentication is connected to frontend.
 
 //use this to return all the flagged posts.
-export function AdminFlaggedPosts() {
+export function AdminFlaggedPosts(props) {
 
+  const { deleteItem, setDeleteItem, handleDeleteToggle } = props
 
   return (
     <div className="flagged-posts">
@@ -71,15 +76,17 @@ export function AdminFlaggedPosts() {
 
       {/* FLAGGED ITEM | POSTS */}
       <div className="flagged-item">
-        <span class="material-symbols-outlined flagged-post-icon">error</span>
-        <span class="material-symbols-outlined flagged-item-close">close</span>
+        <span className="material-symbols-outlined flagged-post-icon">error</span>
+        <span className="material-symbols-outlined flagged-item-close">close</span>
         <div className="post-content">
           <h3>Post_Title</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum bibendum dictum. 
             Nullam dolor sapien, maximus in euismod eu, tempor sagittis nisl. 
             Aenean vitae nisi id nisl ultricies viverra.
           </p>
+          <div className="number-flags">Flag marks: 0</div>
           <Link to="/">Go to Post</Link>
+          <button className="btn delete-btn" onClick={handleDeleteToggle}>Delete Post &nbsp; <span className="material-symbols-outlined delete-close">close</span></button>
         </div>
       </div>
     
@@ -88,24 +95,31 @@ export function AdminFlaggedPosts() {
 }
 
 //use this to return all flagged users
-export function AdminFlaggedUsers() {
+export function AdminFlaggedUsers(props) {
 
-
+  const { deleteItem, setDeleteItem, handleDeleteToggle } = props
+  
   return (
     <div className="flagged-users">
       <h2 className="flagged-title">Flagged Users</h2>
 
       {/* FLAGGED ITEM | USERS */}
       <div className="flagged-item">
-        <span class="material-symbols-outlined flagged-user-icon">gpp_maybe</span>
-        <span class="material-symbols-outlined flagged-item-close">close</span>
+        <span className="material-symbols-outlined flagged-user-icon">gpp_maybe</span>
+        <span className="material-symbols-outlined flagged-item-close">close</span>
         <div className="user-content">
           <div className="flagged-user-profile">{/* Flagged user's profile image */}</div>
           <div className="flagged-user-info">
             <h3>User_name</h3>
             <div className="number-flags">Flag marks: 0</div>
           </div>
-          <button className="btn delete-btn">Delete <span class="material-symbols-outlined delete-close">close</span></button>
+          <button className="btn delete-btn" onClick={handleDeleteToggle}>Delete User &nbsp; <span className="material-symbols-outlined delete-close">close</span></button>
+          {/* Toggle active user status */}
+          {/* <button className="btn delete-btn" onClick={handleDeleteToggle}>
+            {!deleteItem ? 
+            <>Delete User &nbsp; <span className="material-symbols-outlined delete-close">close</span></> 
+          : <>Deleted User</>}
+          </button> */}
         </div>
       </div>
     </div>

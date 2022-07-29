bf799ff23be9bdb94dfdb9543e9c2bee72094c53
Amaar Mohamed <codebyamaar@gmail.com>
2022-07-29 16:45:15 -0500

    fixed weird formatting erros

--- Flora-Fauna-Frontend/src/contexts/auth.jsx
+++ Flora-Fauna-Frontend/src/contexts/auth.jsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ApiClient from "../services/ApiClient.js";
 

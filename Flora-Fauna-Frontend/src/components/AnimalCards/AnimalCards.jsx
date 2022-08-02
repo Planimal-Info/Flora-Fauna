3dbe77a9e7fa0ea211064ba79fc1641221a47441
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-08-02 18:11:42 -0400

    removed old aninmal cards

--- Flora-Fauna-Frontend/src/components/AnimalCards/AnimalCards.jsx
+++ Flora-Fauna-Frontend/src/components/AnimalCards/AnimalCards.jsx
@@ -23,63 +23,6 @@ export default function AnimalCards(props){
       {/* <h2 className="card-common-name">{props.common_name}</h2>
       <h2 classname="card-scientific-name">{props.scientific_name}</h2>
       <button className="animal-moreInfo" onClick={handleOnSubmit}>More Info</button> */}
-      {/* <Card css={{ w: "100%", h: "400px" }}>
-    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-      <Col>
-        <Text size={12} weight="bold" transform="uppercase" color="#000000AA">
-          {props.scientific_name}
-        </Text>
-        <Text h3 color="black">
-          {props.common_name}
-        </Text>
-      </Col>
-    </Card.Header>
-    <Card.Body css={{ p: 0 }}>
-      <Card.Image
-        src={props.picture?.source}
-        width="100%"
-        height="100%"
-        objectFit="cover"
-        alt=""
-      />
-    </Card.Body>
-    <Card.Footer
-      isBlurred
-      css={{
-        position: "absolute",
-        bgBlur: "#ffffff66",
-        borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-        bottom: 0,
-        zIndex: 1,
-      }}
-    >
-      <Row>
-        <Col>
-          <Text color="#000" size={12}>
-            Click button
-          </Text>
-          <Text color="#000" size={12}>
-            to learn more.
-          </Text>
-        </Col>
-        <Col>
-          <Row justify="flex-end">
-            <Button flat auto rounded color="secondary" onClick={handleOnSubmit}>
-              <Text
-                css={{ color: "inherit" }}
-                size={12}
-                weight="bold"
-                transform="uppercase"
-              >
-                Learn More
-              </Text>
-            </Button>
-          </Row>
-        </Col>
-      </Row>
-    </Card.Footer>
-  </Card> */}
-
   <Card>
       <div className="overlay" onClick={handleOnSubmit}></div>
     <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
